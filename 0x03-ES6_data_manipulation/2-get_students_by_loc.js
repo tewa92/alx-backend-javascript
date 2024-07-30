ce73cf7f48803dd782145a498bca662ebb6ee897
tewa92 <tewachewsaleigizer@gmail.com>
2024-07-30 15:06:11 -0700

    data manipulation

--- 0x03-ES6_data_manipulation/2-get_students_by_loc.js
+++ 0x03-ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,7 +1,4 @@
 export default function getStudentsByLocation(students, location) {
-  if (!(students instanceof Array)) {
-      return [];
-  }
   const result = students.filter((student) => (student.location === location));
   return result;
 }
