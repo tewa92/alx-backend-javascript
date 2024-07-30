bea4174cab5d3666d0938f97b6b9807393f6ffab
tewa92 <tewachewsaleigizer@gmail.com>
2024-07-30 03:05:10 -0700

    data_manipulation

--- 0x03-ES6_data_manipulation/0-get_list_students.js
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -1,10 +1,10 @@
-/*
- * return a list of student 
+/**
+ * return a list of student
  */
 export default function getListStudents() {
     return [
-        { id: 1, firstName: "Guillaume", location: "San Francisco" },
-        { id: 2, firstName: "James", location: "Columbia" },
-        { id: 5, firstName: "Serena", location: "San Francisco" }
-    ];
+        { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+        { id: 2, firstName: 'James', location: 'Columbia' },
+        { id: 5, firstName: 'Serena', location: 'San Francisco' }
+    ]
 }
