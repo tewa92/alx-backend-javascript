2cd4e29e00b6a02feb3376ca140dae77fbbd120d
tewa92 <tewachewsaleigizer@gmail.com>
2024-07-30 15:51:10 -0700

    data manipulation

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,4 +1,4 @@
 export default function getStudentIdsSum(students) {
-    const sum = students.reduce((a, b) => (a + b.id), 0);
-    return sum
+  const sum = students.reduce((a, b) => (a + b.id), 0);
+  return sum;
 }
