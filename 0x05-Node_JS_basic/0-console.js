6338360497db97dbfd9ec3974bafcd999b9ec5f1
tewa92 <tewachewsaleigizer@gmail.com>
2024-09-18 17:18:18 +0300

    node

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,9 @@
+/**
+ * Displays a message that prints in STDOUT.
+ * @param {String} msg : message to display.
+ */
+const displayMessage = (msg) => {
+    console.log(msg);
+};
+
+module.exports = displayMessage;
